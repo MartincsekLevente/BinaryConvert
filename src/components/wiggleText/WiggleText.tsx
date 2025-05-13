3fbd949db6a38f866c5c2c371fed2ff7130c7f08
Martincsek Levente <martincsekl@gmail.com>
2025-05-13 16:17:17 +0200

    Fix wiggle text animation

--- src/components/wiggleText/WiggleText.tsx
+++ src/components/wiggleText/WiggleText.tsx
@@ -7,14 +7,24 @@ interface WiggleTextProps {
 
 export default function WiggleText({text}: WiggleTextProps) {
     return (
-        <motion.div className="wiggle-text-container">
+        <div className="wiggle-text-container">
             {text.split("").map((char, index) => (
                 <motion.span
+                    className="wiggle-text-char"
                     key={index}
+                    animate={{
+                        y: [0, 10, 0, -10, 0],
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 0.9,
+                        delay: index * 0.05,
+                        ease: "linear"
+                    }}
                 >
                     {char}
                 </motion.span>
             ))}
-        </motion.div>
+        </div>
     );
 }
